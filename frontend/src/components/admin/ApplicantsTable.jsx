78147340c0684ecbcafe55bf0adb77f982f55e0d
Shriyog <pundeshriyog2004@gmail.com>
2026-08-19 23:06:30 +0530

    added accept reject feature

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import { MoreHorizontal } from 'lucide-react'
+import { useSelector } from 'react-redux'
+import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { toast } from 'sonner'
 
 const shortListingStatus = ["Accepted", "Rejected"]
 const ApplicantsTable = () => {
+    const {applicants} = useSelector(store => store.application);
+
+    const statusHandler = async(status, id) => {
+        try {
+            axios.defaults.withCredentials = true;
+            const res = await axios.post(`${APPLICATION_API_END_POINT}/status/${id}/update`, {status},);
+            if(res.data.success){
+                toast.success(res.data.message)
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
+
     return (
         <div>
             <Table>
@@ -20,12 +38,18 @@ const ApplicantsTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <tr>
-                        <TableCell>Full Name</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Contact</TableCell>
-                        <TableCell>Resume</TableCell>
-                        <TableCell>Date</TableCell>
+                    {
+                        applicants && applicants?.applications?.map((item)=>(
+                            <tr key={item._id}>
+                        <TableCell>{item?.applicant?.fullname}</TableCell>
+                        <TableCell>{item?.applicant?.email}</TableCell>
+                        <TableCell>{item?.applicant?.phonenumber}</TableCell>
+                        <TableCell className="text-blue-600 cursor-pointer">
+                            {
+                                item?.applicant?.profile?.resume ? <a href={item?.applicant?.profile?.resume} target="_blank">{item?.applicant?.profile?.resumeOriginalName}</a> : <span>NA</span>
+                            }
+                        </TableCell>
+                        <TableCell>{item?.applicant?.createdAt.split("T")[0]}</TableCell>
                         <TableCell className="float-right cursor-pointer">
                             <Popover>
                                 <PopoverTrigger>
@@ -35,7 +59,7 @@ const ApplicantsTable = () => {
                                     {
                                         shortListingStatus.map((status, index) => {
                                             return (
-                                                <div key={index} className='flex w-fit items-center my-2 cursor-pointer '>
+                                                <div onClick={() => statusHandler(status, item?._id)} key={index} className='flex w-fit items-center my-2 cursor-pointer '>
                                                     <span>{status}</span>
                                                 </div>
                                             )
@@ -45,6 +69,9 @@ const ApplicantsTable = () => {
                             </Popover>
                         </TableCell>
                     </tr>
+                        ))
+                    }
+                    
                 </TableBody>
 
             </Table>
